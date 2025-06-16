214a835578f2901174c8b26fd71f9b942a9b020b
QuangHung <hung82390@gmail.com>
2025-06-16 19:24:33 +0700

    test db

---
+++ src/main/java/adn_web/java_project/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package adn_web.java_project.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("DNA Testing Service API")
+                        .version("1.0")
+                        .description("API documentation for DNA Testing Service Management System")
+                        .contact(new Contact()
+                                .name("DNA Testing Service")
+                                .email("contact@dnatesting.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+} 
---
+++ src/main/java/adn_web/java_project/model/User.java
@@ -0,0 +1,38 @@
+package adn_web.java_project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.GUEST;
+
+    public enum Role {
+        GUEST, CUSTOMER, STAFF, MANAGER, ADMIN
+    }
+} 
